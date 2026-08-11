010f31288c0922598abcd82e89e4aca326b971d4
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-08-11 10:57:24 -0300

    Add files via upload

---
+++ leia idade! se maior 18 aprovado senao reprovado.cpp
@@ -0,0 +1,22 @@
+// 1.Nome:Leia idade! se maior 18 "entrada permitida"
+#include<stdio.h>>
+
+int idade;
+
+
+
+int main (){
+
+printf("qual sua idade? \n");
+scanf("%d", &idade);
+
+ if (idade >= 18) { printf ("Entrada PERMITIDA");
+ } else { printf ("Entrada NEGADA");
+ }
+  return 0;
+
+
+
+
+
+}
