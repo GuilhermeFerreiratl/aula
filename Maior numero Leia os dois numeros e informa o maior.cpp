6e74244cc0d99346cd9a66eef7fd32f1066bfa0f
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-08-11 11:08:00 -0300

    Add files via upload

---
+++ Maior numero Leia os dois numeros e informa o maior.cpp
@@ -0,0 +1,27 @@
+// 1.Nome:Maior numero: leia os dois e inform o maior.
+#include<stdio.h>>
+
+int x;
+int y;
+
+
+int main (){
+
+printf("primeiro numero? \n");
+scanf("%d", &x);
+
+printf("segundo numero? \n");
+scanf("%d", &y);
+
+if (x >= y) { printf (" seu maior numero e %d", x);
+ } else { printf ("seu maior numero e %d", y);
+ 
+
+ }
+  return 0;
+
+
+
+
+
+}
