2c0d268198b92e101bec1c6d452201afcf46680d
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-08-11 11:26:09 -0300

    Update Maior numero Leia os dois numeros e informa o maior.cpp

--- Maior numero Leia os dois numeros e informa o maior.cpp
+++ Maior numero Leia os dois numeros e informa o maior.cpp
@@ -13,8 +13,13 @@ scanf("%d", &x);
 printf("segundo numero? \n");
 scanf("%d", &y);
 
-if (x >= y) { printf (" seu maior numero e %d", x);
- } else { printf ("seu maior numero e %d", y);
+if (x > y) { printf (" seu maior numero e %d", x);
+ }
+ else if (x == y) { 
+ printf ("os numeros sao igual");
+}
+ 
+  else { printf ("seu maior numero e %d", y);
  
 
  }
