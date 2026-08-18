67d0217a17fd86c80bb4bb16e629414221d8dfa9
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-08-18 10:02:50 -0300

    Add files via upload

---
+++ PAR E IMPAR.cpp
@@ -0,0 +1,27 @@
+// 1.Nome:PAR E IMPAR
+#include<stdio.h>>
+
+int x;
+
+
+
+int main (){
+
+printf("primeiro numero? \n");
+scanf(" %d", &x);
+
+
+if (x % 2 == 0) {
+	printf ("PAR, %d e par : %d", x);
+}
+else { 
+printf ("IMPAR, %d e impar : %d", x);
+   return 0;
+
+ }
+}
+
+
+
+
+
