ee092de34346d7903dd07a7d6b7a5b9c409c2bd3
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-08-18 09:11:33 -0300

    Add files via upload

---
+++ MAIOR MENOR E ZERO.cpp
@@ -0,0 +1,24 @@
+  // MAIOR MENOR E ZERO
+#include<stdio.h>
+int palpite;
+
+int main (){
+	printf ("Qual valor deseja saber :");
+	scanf("%d", &palpite);
+	
+	if(palpite > 0)
+	{printf("MAIOR");
+	}
+	else if (palpite < 0){
+		printf("MENOR");
+	}
+	else {
+		printf("ZERO");
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
