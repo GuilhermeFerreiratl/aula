2c15ef8a1c72e5b467d9897d4f5792076893aa48
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-08-18 10:45:47 -0300

    Add files via upload

---
+++ salario aumento porcentagem.cpp
@@ -0,0 +1,25 @@
+// 1.Nome:SALARIO +10%
+#include<stdio.h>>
+
+int x;
+int y;
+int q;
+int w;
+int u;
+
+int main (){
+
+printf("Digite seu salario! \n");
+scanf(" %d", &x);
+printf("Digite o aumento! \n");
+scanf(" %d", &q);
+
+y = (x / 100 );
+w = (y * q);
+u = (w + x);
+
+	printf ("%d salario com aumento \n", u);
+    printf ("%d salario sem aumento \n", x);
+
+
+ }
