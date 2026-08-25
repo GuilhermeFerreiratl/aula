d7f58d6ed1cd505de933421847991fd5fe4ac3d8
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-08-25 11:31:07 -0300

    Create imcompleto aula fazer sozinho um sistema com funcoes aprendida.cpp

---
+++ imcompleto aula fazer sozinho um sistema com funcoes aprendida.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+float numero;
+char nome;
+int vaor;
+int cartao;
+float valor;
+float x;
+int dim = 10;
+
+
+int main() {
+	
+	
+	printf("Oque voce gostaria?");
+	scanf ("%s", &nome);
+	
+
+	
+	printf("quantos gostaria?");
+	scanf ("%f", &valor);
+	
+	
+  
+    
+    printf("Digite o valor do produto: ");
+    scanf("%f", &x); 
+
+numero =  (x * valor );
+
+    printf("total : %.2f \n", numero);
+
+
+printf("forma pagamento? ");
+printf("1=Debito");
+printf("2=credito");
+printf("3=pix");
+scanf("%d", &dim);
+
+{
+
+switch (dim) {
+	
+	case 1:
+	printf("Debito");
+	break;
+	case 2:
+	printf("credito");
+	break;
+	case 3:
+	printf("pix");
+	break;
+
+
+}
+}
+ dim  = (numero);
+ dim  = ((numero/10)+numero);
+ dim = (numero - (numero/10);
+
+
+
+
+
+    return 0;
+}
