a3f2cfa424282b438ea25eb7cd1f55a397aab44a
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-08-25 11:27:14 -0300

    Add basic C/C++ function explanations

---
+++ funçoes dev c+c+ linguagem.cpp
@@ -0,0 +1,11 @@
+
+printf = saida de texto
+scanf = entrada de texto
+& = armazena endereço
+int = inteiros para localizar utilize %d
+float = numeros quebrados(Depois virgula) %.2f (duas casas)
+C = Caracter localicar utilize %c
+string = conjunto de caracateres &s
+if = se
+else = senão
+switch case = define resultado 
