7547eda08562a9631f84a43504bfc558582ae71d
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-09-08 11:34:38 -0300

    Add program to input and display five values

---
+++ incompleto repeti numero digitados.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>>
+int valor;
+int valor1;
+int valor2;
+int valor3;
+int valor4;
+int main(){
+	
+printf ("Digite um valor? \n");
+scanf("%d",&valor);
+scanf("%d",&valor1);
+scanf("%d",&valor2);
+scanf("%d",&valor3);
+scanf("%d",&valor4);
+printf("%d" "%d" "%d" "%d" "%d",valor,valor1,valor2,valor3,valor4);	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
